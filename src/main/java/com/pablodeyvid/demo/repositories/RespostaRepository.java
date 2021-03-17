953932f3e7cdd163bcdc8daa775ebbeb312af1d7
Pablo Deyvid <pablodeyvid11@gmail.com>
2021-03-17 00:38:16 -0300

    Repository RespostaRepository created

---
+++ src/main/java/com/pablodeyvid/demo/repositories/RespostaRepository.java
@@ -0,0 +1,9 @@
+package com.pablodeyvid.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pablodeyvid.demo.entities.Resposta;
+
+public interface RespostaRepository extends JpaRepository<Resposta, Long>{
+
+}
